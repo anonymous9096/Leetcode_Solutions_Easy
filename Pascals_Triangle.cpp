45f85f88da9da41f387d2ea0a1a0bf9a230642bf
harshd-eep <55032343+harshd-eep@users.noreply.github.com>
2022-10-13 23:17:05 +0530

    Pascals_Triangle.py

--- Pascals_Triangle.cpp
+++ Pascals_Triangle.cpp
@@ -1,26 +1,11 @@
-class Solution {
-public:
-    int ncr(int n,int r)
-    {   int res=1;
-        for(int i=0;i<r;i++)
-        {
-            res=res*(n-i);
-            res=res/(i+1);
-        }
-     return res;
-    }
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>>ans;
-        for(int i=0;i<numRows;i++)
-        {   
-            vector<int>temp;
-            
-            for(int j=0;j<=i;j++)
-            {
-                temp.push_back(ncr(i,j));    
-            }
-            ans.push_back(temp);
-        }
-        return ans;
-    }
-};
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:        
+        def helper(n):
+            if n:
+                helper(n-1)                 # generate above row first
+                ans.append([1]*n)           # insert current row into triangle
+                for i in range(1, n-1):     # update current row values using above row
+                    ans[n-1][i] = ans[n-2][i] + ans[n-2][i-1]
+        ans = []
+        helper(n)
+        return ans
